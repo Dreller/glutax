54ba8ecfa294f12213526c1d5341e5bd16280a90
Dreller <60242965+Dreller@users.noreply.github.com>
2021-04-18 14:12:50 -0400

    Various fixes
    Closes #29

--- page.php
+++ page.php
@@ -6,7 +6,7 @@ include('php/gtInclude.php');
 <head>
 <title><?= _NAME ?></title>
 <script>
-    var myLang = "<?= $_SESSION[_SQL_ACC_LANG] ?>";
+    var myLang = "<?php echo $_LANG; ?>";
 </script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="css/bootstrap.min.css" rel="stylesheet">
